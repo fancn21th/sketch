09c8617c286519c4f30c4f9e17fb779d24c255ea
fancn21th <fancn21th@gmail.com>
2024-02-19 16:15:23 +0800

    deploy

--- sketch.js
+++ sketch.js
@@ -1,22 +1,44 @@
 const canvasSketch = require("canvas-sketch");
+// https://github.com/mattdesl/canvas-sketch-util/blob/master/docs/math.md#value--lerpmin-max-t
+const { lerp } = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [2048, 2048],
+  unit: "cm",
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+
+    return points;
+  };
+
+  const points = createGrid();
+  const margin = 400;
+
   return ({ context, width, height }) => {
-    context.fillStyle = "blue";
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
-    context.fillStyle = "red";
-    context.fill();
+    points.forEach(([u, v]) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, width - margin, v);
 
-    context.lineWidth = 20;
-    context.strokeStyle = "green";
-    context.stroke();
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = 20;
+      context.stroke();
+    });
   };
 };
 
